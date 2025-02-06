9f270943a1b7f0dbae11e6b5e1c87a4bff805a97
Jalal <jalal231@hotmail.com>
2025-02-06 05:33:01 +0000

    tf files

---
+++ terraform-aks-deployment/main.tf
@@ -0,0 +1,6 @@
+module "spn" {
+  source = "./modules/spn"
+}
+module "kv" {
+  source = "./modules/kv"
+}
---
+++ terraform-aks-deployment/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.17.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+
+  features {
+    
+  }
+}
+
+provider "azuread" {
+  # Configuration options
+
+
+}
